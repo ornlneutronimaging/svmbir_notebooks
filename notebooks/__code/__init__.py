aec887760cf2556e9a2f017449141a917110d377
JeanBilheux <bilheuxjm@ornl.gov>
2024-10-03 12:10:43 -0400

    turn off debug mode. this refs #3

--- notebooks/__code/__init__.py
+++ notebooks/__code/__init__.py
@@ -7,7 +7,7 @@ class DataType:
     cleaned_images = 'cleaned images'
 
 
-DEBUG = True
+DEBUG = False
 debug_folder = {DataType.sample: "/SNS/VENUS/IPTS-33699/shared/autoreduce/mcp/September20_2024_PurpleCar_GoldenRatio_CT_5_0_C_Cd_inBeam_Resonance",
                 DataType.ob: "/SNS/VENUS/IPTS-33699/shared/autoreduce/mcp/September19_2024_OpenBeam_28_0_C_Resonance",
                 DataType.cleaned_images: '/SNS/VENUS/IPTS-33699/shared/processed_data/jean_test',
