dd6413c797ba53ab24e6c373e545e3574f731fc3
JeanBilheux <bilheuxjm@ornl.gov>
2024-10-25 13:58:27 -0400

    added root of bash script. this refs #22

---
+++ notebooks/svmbir_reconstruction_cli.py
@@ -0,0 +1,22 @@
+import argparse
+import json
+import os
+import logging
+
+from __code.utilities.time import get_current_time_in_special_file_name_format
+from __code.utilities.logging import setup_logging
+from __code.utilities.json import load_json
+
+setup_logging("svmbir_reconstruction_cli")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run the full svmbir workflow using the provided config file")
+    parser.add_argument('config_file', type=str, nargs=1, help="JSON config file created by notebook")
+    args = parser.parse_args()
+
+    config_file_name = args.config_file[0]
+    logging.info(f"loading config file name: {config_file_name}")
+    config_dictionary = load_json(config_file_name)
+    logging.info(f"{config_dictionary}")    
